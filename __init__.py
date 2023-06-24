4cebbd253aa020d966410a5613a6066e30c8e8b5
pachinco <holger.schanz@me.com>
2023-06-23 20:38:52 -0700

    x

--- __init__.py
+++ __init__.py
@@ -121,8 +121,8 @@ class Mkz(MycroftSkill):
     @intent_file_handler('hmi.show.intent')
     def handle_show_hmi(self, message):
         uiIdx = 0
-        for k,v in message.data["hmi"]:
-            self.log.info('handle_show_hmi: %s:%s' % (k, v))
+        for k in message.data["hmi"]:
+            self.log.info('handle_show_hmi: %s:%s' % k)
             if k in self.uiIdx:
                 uiIdx |= v
         self.log.info('handle_show_hmi: uiIdx=%d' % uiIdx)
